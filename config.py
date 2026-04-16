a5be58640d085965e23d2f94faf2f6b3fafec1ae
febrianhikmah <febriannurrohim12@gmail.com>
2026-04-17 01:03:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 API_KEY = "AIzaSyB1duIkhY6t032IpNTGGzQQ9vngA2ihE_g"
 
 DB_CONFIG = {
-    "host": "localhost",
-    "database": "youtube_dw",
+    "host": "db.xcqkcjvrawrorjnsgeew.supabase.co",
+    "database": "postgres",
     "user": "postgres",
-    "password": "Febrian02",
+    "password": "lUyPEvZ6rOFtrSJA",
     "port": 5432
 }
